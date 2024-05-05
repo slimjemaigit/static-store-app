c5651bbce600f909fddebe408d08cba69b92b180
slimjemaigit <slim.speedknees@gmail.com>
2024-05-05 20:06:39 +0100

    adding static foods to home page

---
+++ src/app/components/pages/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+import { FoodService } from '../../../services/food.service';
+import { Food } from '../../../shared/models/Food';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  foods:Food[] = [];
+  constructor(private foodService:FoodService) {
+    this.foods = foodService.getAll();
+  }
+
+  ngOnInit(): void {
+  }
+
+}
